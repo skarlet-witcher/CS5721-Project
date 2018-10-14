f17eb8ff12eb7ef765077ccb8abde580b5841eec
Xiangkai Tang <empathytxk@hotmail.com>
2018-10-14 16:50:09 +0100

    Delete old GUI

--- bank-client/src/main/java/BankClient.java
+++ bank-client/src/main/java/BankClient.java
@@ -1,14 +1,10 @@
-import view.CustomerLoginView;
-import view.CustomerMainView;
-import view.MainMenu;
-
 public class BankClient {
 
     public static void main(String[] args) {
 
         try {
             // new CustomerLoginView().run();
-            new CustomerMainView().run();
+            // new CustomerMainView().run();
         } catch (Exception e) {
             e.printStackTrace();
         }
