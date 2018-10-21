f79299fff45292bb7ba138e5dbb348aa0d41f4e4
orangejuice <cherrymeteor@outlook.com>
2018-10-21 14:49:34 +0100

    implement of user login request rpc

---
+++ bank-client-selfservice/src/main/java/rpc/client/CustomerLoginRpc.java
@@ -0,0 +1,49 @@
+package rpc.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import rpc.Response;
+import rpc.UserCustomerLoginGrpc;
+import rpc.UserLoginReqReply;
+import rpc.UserLoginReqRequest;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static Const.Server.SERVER_HOST;
+import static Const.Server.SERVER_PORT;
+
+public class CustomerLoginRpc {
+    private static final Logger logger = Logger.getLogger(CustomerLoginRpc.class.getName());
+
+    private static CustomerLoginRpc instance = null;
+
+    public static CustomerLoginRpc getInstance() {
+        if (instance == null) {
+            return new CustomerLoginRpc();
+        }
+        return instance;
+    }
+
+    public UserLoginReqReply loginReq(UserLoginReqRequest loginReqRequest) throws Exception {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
+                .usePlaintext().build();
+        UserCustomerLoginGrpc.UserCustomerLoginBlockingStub blockingStub = UserCustomerLoginGrpc.newBlockingStub(channel);
+
+        logger.info(loginReqRequest.getUserId() + " is requesting to login.");
+
+        Response response = blockingStub.loginReq(loginReqRequest);
+
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+
+        if (response.getStatusCode() == 200) {
+            logger.info(loginReqRequest.getUserId() + " login request detail check successful.");
+            return response.getUserLoginReqReply();
+        } else {
+            logger.info(loginReqRequest.getUserId() + " login request fail due to " + response.getDescription());
+            throw new Exception(response.getDescription());
+        }
+
+    }
+
+}
