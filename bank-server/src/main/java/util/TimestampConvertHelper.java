79c5c1c651fa3d3493061ec0e1207a2ca713f222
orangejuice <cherrymeteor@outlook.com>
2018-10-23 22:20:57 +0100

    timestamp convert util

--- bank-server/src/main/java/util/TimestampConvertHelper.java
+++ bank-server/src/main/java/util/TimestampConvertHelper.java
@@ -4,13 +4,13 @@ import java.sql.Timestamp;
 
 public class TimestampConvertHelper {
 
-    public static Timestamp RpcToMysql(com.google.protobuf.Timestamp rpcTime) {
+    public static Timestamp rpcToMysql(com.google.protobuf.Timestamp rpcTime) {
         Timestamp timestamp = new Timestamp(0);
         timestamp.setNanos(rpcTime.getNanos());
         return timestamp;
     }
 
-    public static com.google.protobuf.Timestamp MysqlToRpc(Timestamp mysqlTime) {
+    public static com.google.protobuf.Timestamp mysqlToRpc(Timestamp mysqlTime) {
         return com.google.protobuf.Timestamp.newBuilder()
                 .setNanos(mysqlTime.getNanos()).build();
     }
