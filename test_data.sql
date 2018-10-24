72e292f36a8c4a4593b3fd9d4725b3679339ff20
Xiangkai Tang <empathytxk@hotmail.com>
2018-10-24 16:12:28 +0100

    test data

---
+++ test_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `bank_staff` VALUES (1,1000000000,'123456','lu','hao','sb@qq.com',1);
+
+INSERT INTO `user_account_type` VALUES (1,'Current account','',18,200,1,1,0,0.1,0.25,1,3000,5,30,12),
+(2,'Student current account','',16,200,1,1,1,0,0,0,0,0,0,0),
+(3,'Young saver account','',6,18,1,0,0,0,0,0,0,0,5,0),
+(4,'Golden elder account','',66,200,1,1,0,0,0,0,0,0,5,0);
+
+INSERT INTO `user` VALUES (1,1000000000,'123123','234','xiangkai','tang',1,'1996-03-23 00:00:00','ul','haha@qq.com','123333222',1),
+(2,1000000001,'321321','321','hao','lu',2,'2018-01-01 00:00:00','hell','shit@qq.com','17481748',1);
+
+INSERT INTO `user_payee` VALUES (1,'xiangkai','IE12BOFI23341234000000',1);
