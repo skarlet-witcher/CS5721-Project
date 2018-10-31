b04b82936d62abdd7733158c1670fcbc7fdece65
orangejuice <cherrymeteor@outlook.com>
2018-10-31 11:45:02 +0000

    fix

--- bank-core/src/main/java/Const/UserAccountType.java
+++ bank-core/src/main/java/Const/UserAccountType.java
@@ -5,4 +5,19 @@ public class UserAccountType {
     public static final int STUDENT_ACCOUNT = 2;
     public static final int YOUNG_SAVER_ACCOUNT = 3;
     public static final int GOLDEN_ACCOUNT = 4;
+
+    public static String getTypeName(int typeNum) {
+        switch (typeNum) {
+            case 1:
+                return "Current Account";
+            case 2:
+                return "Student Account";
+            case 3:
+                return "Young Saver Account";
+            case 4:
+                return "Golden Account";
+            default:
+                return "Other Account";
+        }
+    }
 }
