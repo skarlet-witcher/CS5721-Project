df5aa276f622c33d841ef5393d593a0aa369e7fb
Ashly Mathew <ashlymathew93@gmail.com>
2018-11-19 18:52:01 +0000

    Strategy pattern

--- bank-server/src/main/java/service/impl/UserCustomerApplyService.java
+++ bank-server/src/main/java/service/impl/UserCustomerApplyService.java
@@ -7,7 +7,7 @@ import dao.impl.UserDao;
 import entity.UserApplyArchiveEntity;
 import entity.UserEntity;
 import service.IUserCustomerApplyService;
-import util.FaultFactory;
+import util.*;
 
 import java.sql.Timestamp;
 import java.util.Date;
@@ -31,37 +31,90 @@ public class UserCustomerApplyService implements IUserCustomerApplyService {
     @Override
     public void requestPersonalAccountApply(String firstName, String lastName, String identityNum, int identityType, int accountType, int cardType,
                                             Timestamp birthDate, int gender, String address, String email, String phone, int isNewUser, Long userId) throws Exception {
+        IAccounts newAccount=new PersonalAccount();
+        if(newAccount.checkAgeValidity(birthDate)) {
+
         try {
             // basic info
-            UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
-            UserApplyArchiveEntity.setFirstName(firstName);
-            UserApplyArchiveEntity.setLastName(lastName);
-            UserApplyArchiveEntity.setIdentityId(identityNum);
-            UserApplyArchiveEntity.setIdentityIdType(identityType);
-            UserApplyArchiveEntity.setAccountType(accountType);
-            UserApplyArchiveEntity.setCardType(cardType);
-            UserApplyArchiveEntity.setBirthDate(birthDate);
-            UserApplyArchiveEntity.setGender(gender);
-            UserApplyArchiveEntity.setAddress(address);
-            UserApplyArchiveEntity.setEmail(email);
-            UserApplyArchiveEntity.setPhone(phone);
-            UserApplyArchiveEntity.setApplyTime(new Timestamp(new Date().getTime()));
-            UserApplyArchiveEntity.setStatus(0);
-            UserApplyArchiveEntity.setNewUserApply(isNewUser);
-            UserApplyArchiveEntity.setUserId(userId);
 
-            userApplyDao.requestAccountApply(UserApplyArchiveEntity);
+                UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
+                UserApplyArchiveEntity.setFirstName(firstName);
+                UserApplyArchiveEntity.setLastName(lastName);
+                UserApplyArchiveEntity.setIdentityId(identityNum);
+                UserApplyArchiveEntity.setIdentityIdType(identityType);
+                UserApplyArchiveEntity.setAccountType(accountType);
+                UserApplyArchiveEntity.setCardType(cardType);
+                UserApplyArchiveEntity.setBirthDate(birthDate);
+                UserApplyArchiveEntity.setGender(gender);
+                UserApplyArchiveEntity.setAddress(address);
+                UserApplyArchiveEntity.setEmail(email);
+                UserApplyArchiveEntity.setPhone(phone);
+                UserApplyArchiveEntity.setApplyTime(new Timestamp(new Date().getTime()));
+                UserApplyArchiveEntity.setStatus(0);
+                UserApplyArchiveEntity.setNewUserApply(isNewUser);
+                UserApplyArchiveEntity.setUserId(userId);
+
+                userApplyDao.requestAccountApply(UserApplyArchiveEntity);
+
+
         } catch (Exception e) {
-            throw FaultFactory.throwFaultException("Fail to apply a personal current account");
+           throw FaultFactory.throwFaultException("Fail to apply a personal current account");
         }
+
+        }
+        else
+            throw FaultFactory.throwFaultException("Below 18");
     }
 
     @Override
     public void requestStudentAccountApply(String firstName, String lastName, String identityNum, int identityType, int accountType, int cardType, Timestamp birthDate, int gender,
                                            String address, String email, String phone, int isNewUser,
                                            Timestamp graduateDate, String studentId, String university, Long userId) throws Exception {
+        IAccounts newAccount=new StudentAccount();
+        if(newAccount.checkAgeValidity(birthDate)) {
+
         try {
+
+                UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
+                UserApplyArchiveEntity.setFirstName(firstName);
+                UserApplyArchiveEntity.setLastName(lastName);
+                UserApplyArchiveEntity.setIdentityId(identityNum);
+                UserApplyArchiveEntity.setIdentityIdType(identityType);
+                UserApplyArchiveEntity.setAccountType(accountType);
+                UserApplyArchiveEntity.setCardType(cardType);
+                UserApplyArchiveEntity.setBirthDate(birthDate);
+                UserApplyArchiveEntity.setGender(gender);
+                UserApplyArchiveEntity.setAddress(address);
+                UserApplyArchiveEntity.setEmail(email);
+                UserApplyArchiveEntity.setPhone(phone);
+                UserApplyArchiveEntity.setApplyTime(new Timestamp(new Date().getTime()));
+                UserApplyArchiveEntity.setNewUserApply(isNewUser);
+                UserApplyArchiveEntity.setUserId(userId);
+                UserApplyArchiveEntity.setStatus(0);
+                // student info
+                UserApplyArchiveEntity.setGraduateDate(graduateDate);
+                UserApplyArchiveEntity.setStudentId(studentId);
+                UserApplyArchiveEntity.setUniversity(university);
+
+                userApplyDao.requestAccountApply(UserApplyArchiveEntity);
+
+
+        } catch (Exception e) {
+            throw FaultFactory.throwFaultException("Fail to apply a student current account");
+        }
+        }
+        else
+            throw FaultFactory.throwFaultException("Student ashould be above 16 years of age");
+    }
+
+    @Override
+    public void requestYoungSaverAccountApply(String firstName, String lastName, String identityNum, int identityType, int accountType, int cardType, Timestamp birthDate, int gender, String address, String email, String phone, int isNewUser, Long parentUserId, String parentFirstName, String parentLastName, long userId) throws Exception {
+
+        IAccounts newAccount=new YoungSaversAccount();
+        if(newAccount.checkAgeValidity(birthDate)) {
             UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
+            UserEntity result = null;
+            UserEntity UserEntity = new UserEntity();
             UserApplyArchiveEntity.setFirstName(firstName);
             UserApplyArchiveEntity.setLastName(lastName);
             UserApplyArchiveEntity.setIdentityId(identityNum);
@@ -77,63 +130,37 @@ public class UserCustomerApplyService implements IUserCustomerApplyService {
             UserApplyArchiveEntity.setNewUserApply(isNewUser);
             UserApplyArchiveEntity.setUserId(userId);
             UserApplyArchiveEntity.setStatus(0);
-            // student info
-            UserApplyArchiveEntity.setGraduateDate(graduateDate);
-            UserApplyArchiveEntity.setStudentId(studentId);
-            UserApplyArchiveEntity.setUniversity(university);
+            // young saver info
+            UserApplyArchiveEntity.setParentUserId(parentUserId);
+            UserApplyArchiveEntity.setParentFirstName(parentFirstName);
+            UserApplyArchiveEntity.setParentLastName(parentLastName);
 
+            // check parent info
+            UserEntity.setUserId(parentUserId);
+            UserEntity.setFirstName(parentFirstName);
+            UserEntity.setLastName(parentLastName);
+            try {
+                result = userDao.selectUserByIdAndName(parentUserId, parentFirstName, parentLastName);
+            } catch (Exception e) {
+                throw FaultFactory.throwFaultException("Fail to apply a young saver account");
+            }
+            if (result == null) {
+                throw FaultFactory.throwFaultException("parent info does not exist!");
+            }
             userApplyDao.requestAccountApply(UserApplyArchiveEntity);
-
-        } catch (Exception e) {
-            throw FaultFactory.throwFaultException("Fail to apply a student current account");
         }
-    }
-
-    @Override
-    public void requestYoungSaverAccountApply(String firstName, String lastName, String identityNum, int identityType, int accountType, int cardType, Timestamp birthDate, int gender, String address, String email, String phone, int isNewUser, Long parentUserId, String parentFirstName, String parentLastName, long userId) throws Exception {
-
-        UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
-        UserEntity result = null;
-        UserEntity UserEntity = new UserEntity();
-        UserApplyArchiveEntity.setFirstName(firstName);
-        UserApplyArchiveEntity.setLastName(lastName);
-        UserApplyArchiveEntity.setIdentityId(identityNum);
-        UserApplyArchiveEntity.setIdentityIdType(identityType);
-        UserApplyArchiveEntity.setAccountType(accountType);
-        UserApplyArchiveEntity.setCardType(cardType);
-        UserApplyArchiveEntity.setBirthDate(birthDate);
-        UserApplyArchiveEntity.setGender(gender);
-        UserApplyArchiveEntity.setAddress(address);
-        UserApplyArchiveEntity.setEmail(email);
-        UserApplyArchiveEntity.setPhone(phone);
-        UserApplyArchiveEntity.setApplyTime(new Timestamp(new Date().getTime()));
-        UserApplyArchiveEntity.setNewUserApply(isNewUser);
-        UserApplyArchiveEntity.setUserId(userId);
-        UserApplyArchiveEntity.setStatus(0);
-        // young saver info
-        UserApplyArchiveEntity.setParentUserId(parentUserId);
-        UserApplyArchiveEntity.setParentFirstName(parentFirstName);
-        UserApplyArchiveEntity.setParentLastName(parentLastName);
-
-        // check parent info
-        UserEntity.setUserId(parentUserId);
-        UserEntity.setFirstName(parentFirstName);
-        UserEntity.setLastName(parentLastName);
-        try {
-            result = userDao.selectUserByIdAndName(parentUserId, parentFirstName, parentLastName);
-        } catch (Exception e) {
-            throw FaultFactory.throwFaultException("Fail to apply a young saver account");
-        }
-        if (result == null) {
-            throw FaultFactory.throwFaultException("parent info does not exist!");
-        }
-        userApplyDao.requestAccountApply(UserApplyArchiveEntity);
+        else
+            throw FaultFactory.throwFaultException("Age should be between 6 and 16 years of age for a young savers account");
     }
 
     @Override
     public void requestGoldenAccountApply(String firstName, String lastName, String identityNum, int identityType, int accountType, int cardType,
                                           Timestamp birthDate, int gender, String address, String email, String phone, int isNewUser, Long userId) throws Exception {
+        IAccounts newAccount=new GoldenAccount();
+        if(newAccount.checkAgeValidity(birthDate)) {
+
         try {
+
             // basic info
             UserApplyArchiveEntity UserApplyArchiveEntity = new UserApplyArchiveEntity();
             UserApplyArchiveEntity.setFirstName(firstName);
@@ -155,6 +182,10 @@ public class UserCustomerApplyService implements IUserCustomerApplyService {
         } catch (Exception e) {
             throw FaultFactory.throwFaultException("Fail to apply a golden current account");
         }
+
+    }
+            else
+                    throw FaultFactory.throwFaultException("Age should be above 66 years of age");
     }
 
     @Override
---
+++ bank-server/src/main/java/util/PersonalAccount.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by user on 11/11/2018.
+ */
+public class PersonalAccount implements IAccounts {
+
+    public boolean checkAgeValidity(Timestamp dob){
+
+        Timestamp curdate=new Timestamp(System.currentTimeMillis());
+        String curYear=curdate.toString().substring(0,4);
+        String birthYear=dob.toString().substring(0,4);
+        Integer curInt= Integer.valueOf(curYear);
+        Integer birInt=Integer.valueOf(birthYear);
+
+        if((curInt-birInt) >18 )
+            return true;
+
+        return false;
+    }
+}
